0b081e5b37ade4dfe3e82b8702fca2691a7bb22e
Vlad Boyarkin <sov7.sov7@yandex.ru>
2021-11-22 05:55:56 +0300

    скелет без функционала, + calc.cpp, calc.h, function.cpp

---
+++ calc.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include "calc.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	int choice = 0;
+	double a, b;
+
+	cout << "Выберите операцию:" << endl;
+	cout << "1 - сложение" << endl;
+	cout << "2 - вычитание" << endl;
+	cout << "3 - умножение" << endl;
+	cout << "4 - деление" << endl;
+	cin >> choice;
+	cout << endl;
+
+	cout << "Введите числа a b:" << endl;
+	cin >> a >> b;
+	cout << endl;
+
+	switch (choice) {
+	default:
+		cout << "ERROR" << endl;
+		break;
+
+	case 1:
+		summation(a, b);
+		break;
+
+	case 2:
+		subtraction(a, b);
+		break;
+
+	case 3:
+		multiplication(a, b);
+		break;
+
+	case 4:
+		division(a, b);
+		break;
+
+	}
+	
+	system("pause");
+	return 0;
+}
---
+++ function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "calc.h"
+using namespace std;
+
+double summation(double a, double b) {
+	cout << "a + b = эта операция в разработке" << endl;
+	return 0;
+}
+
+double subtraction(double a, double b) {
+	cout << "a - b = эта операция в разработке" << endl;
+	return 0;
+}
+
+double multiplication(double a, double b) {
+	cout << "a * b = эта операция в разработке" << endl;
+	return 0;
+}
+
+double division(double a, double b) {
+	cout << "a / b = эта операция в разработке" << endl;
+	return 0;
+}
